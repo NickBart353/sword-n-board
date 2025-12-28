dc5d72390643474353c85407da5951840ed671aa
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2025-12-28 18:15:10 +0100

    dungeon entrance, hover text facing camera

--- scripts/building_parts/dungeon_entrance.gd
+++ scripts/building_parts/dungeon_entrance.gd
@@ -7,9 +7,13 @@ func interact():
 	print("entering dungeon...")
 
 func hover():
-	if not $Entrance/BlackSquare/Outline.is_visible():
-		$Entrance/BlackSquare/Outline.set_visible(true)
+	#if not $Entrance/BlackSquare/Outline.is_visible():
+		#$Entrance/BlackSquare/Outline.set_visible(true)
+	if not $Sprite3D.is_visible():
+		$Sprite3D.set_visible(true)
 
 func un_hover():
-	if $Entrance/BlackSquare/Outline.is_visible():
-		$Entrance/BlackSquare/Outline.set_visible(false)
+	#if $Entrance/BlackSquare/Outline.is_visible():
+		#$Entrance/BlackSquare/Outline.set_visible(false)
+	if $Sprite3D.is_visible():
+		$Sprite3D.set_visible(false)
