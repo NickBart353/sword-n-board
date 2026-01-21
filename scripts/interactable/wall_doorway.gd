3070706c7311ffe1f201745b876b3bf1687600e3
bartlett <nicholas_bartlett_30.09@outlook.de>
2026-01-21 15:34:10 +0100

    node3d, viewport, label + sprite inside interactable

--- scripts/interactable/wall_doorway.gd
+++ scripts/interactable/wall_doorway.gd
@@ -14,16 +14,18 @@ func _ready() -> void:
 func hover():
 	#if not $Entrance/BlackSquare/Outline.is_visible():
 		#$Entrance/BlackSquare/Outline.set_visible(true)
-	hovered = true
-	if not $Text_Rotator/Text.is_visible():
-		$Text_Rotator/Text.set_visible(true)
+	#hovered = true
+	#if not $Text_Rotator/Text.is_visible():
+		#$Text_Rotator/Text.set_visible(true)
+	pass
 
 func un_hover():
 	#if $Entrance/BlackSquare/Outline.is_visible():
 		#$Entrance/BlackSquare/Outline.set_visible(false)
-	if hovered:
-		$Text_Rotator/Text.set_visible(false)
-		hovered = false
+	#if hovered:
+		#$Text_Rotator/Text.set_visible(false)
+		#hovered = false
+	pass
 
 func interact():
 	if $AnimationPlayer.is_playing():
@@ -31,12 +33,12 @@ func interact():
 	if open and not in_motion:
 		open = false
 		in_motion = true
-		$Node3D/SubViewport/Label.text = "Press E to open door."
+		#$Node3D/SubViewport/Label.text = "Press E to open door."
 		$AnimationPlayer.play("close_door")
 	elif not open and not in_motion:
 		open = true
 		in_motion = true
-		$Node3D/SubViewport/Label.text = "Press E to close door."
+		#$Node3D/SubViewport/Label.text = "Press E to close door."
 		$AnimationPlayer.play("open_door")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
