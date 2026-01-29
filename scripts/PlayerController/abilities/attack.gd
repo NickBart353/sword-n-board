008e30734f338584b6b5509a0e0d268a89fd3300
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2026-01-29 16:34:09 +0100

    player controller - animations(suck)

--- scripts/PlayerController/abilities/attack.gd
+++ scripts/PlayerController/abilities/attack.gd
@@ -11,6 +11,8 @@ var swing_in_progress: bool = false
 
 func apply_ability(input: Node, movement: Node, abilities: Node, delta: float) -> void:
 	weapon = player.get_equipped_primary()
+	if not weapon is MeleeWeapon: return
+	
 	if not weapon.hit.is_connected(_melee_attack):
 		weapon.hit.connect(_melee_attack)
 	
@@ -30,14 +32,23 @@ func _melee_attack(body, damage):
 		bodies.append(body)
 		body.take_damage(damage)
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "Sword{0}".format([swing]):
-		bodies.clear()
-		swing_in_progress = false
+#func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	#if anim_name == "Sword{0}".format([swing]):
+		#bodies.clear()
+		#swing_in_progress = false
 
 func _on_attack_timer_timeout() -> void:
 	swing = 0
 
-func _on_animation_player_animation_started(anim_name: StringName) -> void:
+#func _on_animation_player_animation_started(anim_name: StringName) -> void:
+	#if anim_name == "Sword{0}".format([swing]):
+		#swinging = false
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Sword{0}".format([swing]):
+		bodies.clear()
+		swing_in_progress = false
+
+func _on_animation_tree_animation_started(anim_name: StringName) -> void:
 	if anim_name == "Sword{0}".format([swing]):
 		swinging = false
--- scripts/PlayerController/animation_controller.gd
+++ scripts/PlayerController/animation_controller.gd
@@ -1,9 +1,12 @@
 extends Node
 
 @onready var anim_player = $"../AnimationPlayer"
+@onready var anim_tree = $"../AnimationTree"["parameters/playback"]
 
 func apply_animations(input: Node, movement: Node, ability: Node, delta: float) -> void:
-	var attack = ability.get_node_or_null("Attack")
-	if attack:
-		if attack.swinging and not anim_player.is_playing():
-			anim_player.play("Sword{0}".format([attack.swing]))
+	if input.dash:
+		var attack = ability.get_node_or_null("Attack")
+		if attack:
+			if attack.swinging and not anim_player.is_playing():
+				anim_tree.travel("Sword{0}".format([attack.swing]))
+				#anim_player.play("Sword{0}".format([attack.swing]))
