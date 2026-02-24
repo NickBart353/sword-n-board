9de5f5089330abd4bac979923fde9b59c52a33f8
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2026-02-24 18:25:49 +0100

    greyboxing + terrain, trying to fix player bug: bouncing while walking down slopes

--- scripts/PlayerController/movement_controller.gd
+++ scripts/PlayerController/movement_controller.gd
@@ -7,6 +7,8 @@ extends Node
 @export var dash_distance: int = 10
 
 var dashing: bool
+var jumping: bool
+var moving: bool
 var dashing_direction: Vector3
 var dashing_origin: Vector3
 
@@ -14,16 +16,25 @@ func apply_movement(input: Node, state_controller: Node, delta: float) -> void:
 	if not input.dash and not dashing:
 		var move_dir := (player.transform.basis * Vector3(input.direction.x, 0, input.direction.y)).normalized()
 		if move_dir:
+			moving = true
 			player.velocity.x = move_dir.x * movement_speed
 			player.velocity.z = move_dir.z * movement_speed
+			#if not jumping:
+				#player.velocity.y = -5
 		else:
+			moving = false
 			player.velocity.x = move_toward(player.velocity.x, 0, movement_speed)
 			player.velocity.z = move_toward(player.velocity.z, 0, movement_speed)
 		
-		#if not player.is_on_floor():
-		player.velocity += player.get_gravity() * delta
+		if not player.is_on_floor():
+			player.velocity += player.get_gravity() * delta
+		if jumping and player.is_on_floor():
+			jumping = false
 		if player.is_on_floor() and input.jump:
 			player.velocity.y += jump_velocity
+			jumping = true
+		#elif not jumping and moving and not dashing:
+			#player.velocity.y += -5
 	else:
 		if not dashing:
 			dashing_origin = player.global_position
