e2d7cbfda45cc27aa24b71b808166746653a1ee0
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2026-02-28 00:01:44 +0100

    dot + ground effect for eaterplant poison attack

--- scripts/VFX/VFX.gd
+++ scripts/VFX/VFX.gd
@@ -5,6 +5,9 @@ class_name Basic_VFX extends Node3D
 
 func _ready() -> void:
 	animation_player.play(animation_name)
+	if not animation_player.animation_finished.is_connected(_on_animation_player_animation_finished):
+		animation_player.animation_finished.connect(_on_animation_player_animation_finished)
+
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == animation_name:
 		queue_free()
---
+++ scripts/components/dot.gd
@@ -0,0 +1,29 @@
+class_name DOT extends Area3D
+
+@export_range(0.0, 10.0) var tick_speed: float = 1.0
+@export var dot_damage: float = 5
+@export var dot_effect_name: String
+
+var timer: Timer
+var overlapping_areas: Array[DOT]
+
+func _ready() -> void:
+	timer = Timer.new()
+	add_child(timer)
+	timer.one_shot = true
+
+func _process(_delta: float) -> void:
+	if not timer.time_left:
+		for area in get_overlapping_areas():
+			if area is DOT and area.dot_effect_name == dot_effect_name:
+				overlapping_areas.append(area)
+		for body in get_overlapping_bodies():
+			if (body is Player or body is Enemy):
+				for dot in overlapping_areas:
+					dot.start_timer_from_overlap()
+				timer.start(tick_speed)
+				body.take_damage(dot_damage, self)
+
+func start_timer_from_overlap():
+	if not timer.time_left:
+		timer.start(tick_speed)
