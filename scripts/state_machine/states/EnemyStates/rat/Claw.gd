dcb6aba653510d46a3f9ba54d90bc0ada62d44d4
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2026-03-09 22:54:36 +0100

    rat claw attack - WIP

---
+++ scripts/state_machine/states/EnemyStates/rat/Claw.gd
@@ -0,0 +1,18 @@
+extends EnemyAbility
+
+@export var claw_scene: PackedScene
+@export var claw_position: Node
+@export var damage: int = 25
+
+func Enter():
+	super()
+	var claw_instance: Area3D = claw_scene.instantiate()
+	add_child(claw_instance)
+	claw_instance.claw_finished.connect(_claw_finished)
+	claw_instance.play_claw(claw_position.global_transform, damage)
+
+func _claw_finished(bite: Area3D):
+	bite.queue_free()
+	cool_down_timer.start()
+	Transitioned.emit(self, "Follow")
+	
