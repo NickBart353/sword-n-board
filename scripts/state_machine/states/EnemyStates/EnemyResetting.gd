719ca9a303aaf11cb17c6e598220597b8284cb2b
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2026-03-11 16:31:36 +0100

    adjustments

--- scripts/state_machine/states/EnemyStates/EnemyResetting.gd
+++ scripts/state_machine/states/EnemyStates/EnemyResetting.gd
@@ -1,21 +1,23 @@
 extends EnemyState
 
 @export var resetting_timer: Timer
+@export var disable_collision_during_reset: bool = true
 
 func Enter():
 	super()
 	resetting_timer.start()
-	enemy.set_collision_mask_value(1, false)
+	if disable_collision_during_reset:
+		enemy.set_collision_mask_value(1, false)
 
 func Exit():
 	super()
 	enemy.velocity = Vector3.ZERO
-	enemy.set_collision_mask_value(1, true)
+	if disable_collision_during_reset:
+		enemy.set_collision_mask_value(1, true)
 
 func Physics_Update(delta: float) -> void:
 	super(delta)
 	enemy.velocity = Vector3(0,3,0)
-	
 
 func _on_resetting_timer_timeout() -> void:
 	Transitioned.emit(self, "Follow")
