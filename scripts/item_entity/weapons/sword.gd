856aa164a45d31b170de056bebb74d562b9d6360
Nogg353 <nicholas_bartlett_30.09@outlook.de>
2026-04-25 08:40:31 +0200

    marker update function for one handed weapons

--- scripts/item_entity/weapons/sword.gd
+++ scripts/item_entity/weapons/sword.gd
@@ -1,8 +1,43 @@
 extends MeleeWeapon
 
+@onready var hand_markers: Node3D = $HandMarkers
+
 @export var sword_player: AudioStreamPlayer3D
 
 signal hit
+
+var marker_positions: Dictionary = {
+	"Hand": {
+		"L_Position": Vector3(-0.035, 0, 0.15),
+		"R_Position": Vector3(0.035, 0, 0.15),
+		"L_Rotation": Vector3(-67.5, -90.0, 90.0),
+		"R_Rotation": Vector3(67.5, -90.0, 90.0),
+		},
+	"Finger": {
+		"L_Position": Vector3(0.044, 0.002, 0.15),
+		"R_Position": Vector3(-0.044, 0.002, 0.15),
+		"L_Rotation": Vector3(-67.5, -90.0, 90.0),
+		"R_Rotation": Vector3(67.5, -90.0, 90.0),
+		},
+	"Thumb": {
+		"L_Position": Vector3(0.024, 0.024, 0),
+		"R_Position": Vector3(-0.024, 0.024, 0),
+		"L_Rotation": Vector3.ZERO,
+		"R_Rotation": Vector3.ZERO,
+		},
+	"FingerPole": {
+		"L_Position": Vector3(-0.98, 0, -1.206),
+		"R_Position": Vector3(0.98, 0, -1.206),
+		"L_Rotation": Vector3.ZERO,
+		"R_Rotation": Vector3.ZERO,
+		},
+	"ThumbPole": {
+		"L_Position": Vector3(0.251, 0.206, 0.316),
+		"R_Position": Vector3(-0.251, 0.206, 0.316),
+		"L_Rotation": Vector3.ZERO,
+		"R_Rotation": Vector3.ZERO,
+		},
+}
 
 func _ready() -> void:
 	pass
@@ -12,5 +47,10 @@ func _on_body_entered(body: Node3D) -> void:
 		hit.emit(body, data.normal_damage)
 		sword_player.play()
 
-func get_weapon_markers() -> Marker3D:
-	return $HandMarkers/HandMarker
+func get_weapon_markers() -> Node3D:
+	return hand_markers
+
+func update_markers(hand: String) -> void:
+	for marker: Marker3D in hand_markers.get_children():
+		marker.position = marker_positions[marker]["{0}_Position".format(hand)]
+		marker.rotation = marker_positions[marker]["{0}_Rotation".format(hand)]
--- scripts/item_entity/weapons/weapon.gd
+++ scripts/item_entity/weapons/weapon.gd
@@ -1,3 +1,5 @@
 @abstract class_name Weapon extends ItemEntity
 
 @abstract func get_weapon_markers()
+
+@abstract func update_markers(hand: String) -> void
