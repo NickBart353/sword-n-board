85117a784a9953c7a0d872e7554a99414e9b3cd4
Nogg353 <nicholas_bartlett_30.09@outlook.de>
2026-05-10 16:21:40 +0200

    katana hand position

--- scripts/item_entity/weapons/katana.gd
+++ scripts/item_entity/weapons/katana.gd
@@ -9,14 +9,14 @@ func _on_body_entered(body: Node3D) -> void:
 func _set_marker_values():
 	marker_positions = {
 		"Hand": {
-			"L_Position": Vector3(-0.035, 0, 0.15),
-			"R_Position": Vector3(0.035, 0, 0.15),
+			"L_Position": Vector3(-0.17, -0.045, 0.03),
+			"R_Position": Vector3(0.108, 0.093, 0.153),
 			"L_Rotation": Vector3(-67.5, -90.0, 90.0),
 			"R_Rotation": Vector3(67.5, -90.0, 90.0),
 			},
 		"Finger": {
-			"L_Position": Vector3(0.044, 0, -0.03),
-			"R_Position": Vector3(-0.044, 0, -0.03),
+			"L_Position": Vector3(0.017, -0.068, -0.019),
+			"R_Position": Vector3(-0.007, 0.133, -0.029),
 			#"L_Position": Vector3(0.044, 0.002, 0.15),
 			#"R_Position": Vector3(-0.044, 0.002, 0.15),
 			"L_Rotation": Vector3(-67.5, -90.0, 90.0),
@@ -25,20 +25,20 @@ func _set_marker_values():
 			#"R_Rotation": Vector3.ZERO,
 			},
 		"Thumb": {
-			"L_Position": Vector3(0.024, 0.024, 0),
-			"R_Position": Vector3(-0.024, 0.024, 0),
+			"L_Position": Vector3(0.058, 0.024, 0.054),
+			"R_Position": Vector3(-0.027, 0.128, 0.049),
 			"L_Rotation": Vector3.ZERO,
 			"R_Rotation": Vector3.ZERO,
 			},
 		"FingerPole": {
-			"L_Position": Vector3(-0.98, 0, -1.206),
-			"R_Position": Vector3(0.98, 0, -1.206),
+			"L_Position": Vector3(-0.035, 0, -0.161),
+			"R_Position": Vector3(0.074, 0.128, -0.096),
 			"L_Rotation": Vector3.ZERO,
 			"R_Rotation": Vector3.ZERO,
 			},
 		"ThumbPole": {
-			"L_Position": Vector3(0.251, 0.206, 0.316),
-			"R_Position": Vector3(-0.251, 0.206, 0.316),
+			"L_Position": Vector3(-0.071, 0.01, 0.11),
+			"R_Position": Vector3(-0.092, 0.13, 0.078),
 			"L_Rotation": Vector3.ZERO,
 			"R_Rotation": Vector3.ZERO,
 			},
