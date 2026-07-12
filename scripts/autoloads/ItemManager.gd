08fb82bfa252c931a6c7925aa3b92551f611786b
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2026-07-12 16:54:36 +0200

    greathammer sounds

--- scripts/autoloads/ItemManager.gd
+++ scripts/autoloads/ItemManager.gd
@@ -201,6 +201,8 @@ func generate_loot(_level: int, _additional_drop_keys: Dictionary = {}):
 	items.append(get_item_from_id("10001"))
 	items.append(get_item_from_id("10002"))
 	items.append(get_item_from_id("10002"))
+	items.append(get_item_from_id("10005"))
+	items.append(get_item_from_id("10005"))
 	
 	var randi: int = randi_range(0, MELEE_WEAPONS.size() - 1)
 	var counter: int = 0
