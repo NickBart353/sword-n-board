b6aa2b54d1880ab410d256a8042203dabf23daa4
NickBart353 <nicholas_bartlett_30.09@outlook.de>
2026-08-05 13:42:41 +0200

    enchant applicator component, fire enchantment

--- scripts/components/enchant_applicator.gd
+++ scripts/components/enchant_applicator.gd
@@ -1,24 +1,64 @@
 @tool
-extends Node3D
-
-@export var weapon: MeleeWeapon
-@export var emission_shape: CollisionShape3D
-@export var particle_effect: GPUParticles3D #export feature is for debug
-@export var weapon_mesh: MeshInstance3D
-@export var mesh_component_names: Array[String]
+class_name EnchantApplicator extends Node3D
 
 const enchant_dict: Dictionary = {
-	"fire": {
-		"particle_effect": "",
+	WeaponData.UPGRADE_TYPE.FIRE: {
+		"particle_effect": preload("uid://x3push4qk0sw"),
 		"color": Color(2.138, 0.589, 0.0),
 	}
 }
 
+enum AMOUNT {small, medium, large}
+
+@export var weapon: MeleeWeapon
+@export var particle_effect: GPUParticles3D #export feature is for debug
+@export var particle_amount: AMOUNT = AMOUNT.small
+@export var weapon_mesh: MeshInstance3D
+@export var mesh_surface_indexes: Array[int] = []
+@export_group("Emission Shapes")
+@export var fire_emission_shape: CollisionShape3D
+
+var upgrade_type: WeaponData.UPGRADE_TYPE
+var vfx_instance: GPUParticles3D
+var emission_shape: CollisionShape3D
+
+#enum UPGRADE_TYPE {NORMAL, MAGIC, FIRE, LIGHTNING, COLD, NATURE, CHAOS}
+
 func _ready() -> void:
-	pass
-	#TODO:
-	#add particle effect as child
-	#get shape from collisionshape
-	#set shape to particle system spawn location
-	#change weapon mesh color to enchanted one 
+	if not weapon or not weapon_mesh or not mesh_surface_indexes:
+		push_error("enchant component not correctly set up for: ", get_parent().data.item_name)
+		return
+	
+	upgrade_type = weapon.data.upgrade_type
+	
+	if upgrade_type == WeaponData.UPGRADE_TYPE.NORMAL:
+		return
+	
+	match upgrade_type:
+		WeaponData.UPGRADE_TYPE.FIRE:
+			emission_shape = fire_emission_shape
+	
+	if not emission_shape or not emission_shape.shape is BoxShape3D:
+		push_error("emission shape not correctly set up for: ", weapon.data.item_name, "Upgrade type", upgrade_type)
+		return
+	
+	vfx_instance = enchant_dict[upgrade_type]["particle_effect"].instantiate()
+	add_child(vfx_instance)
+	
+	
+	match particle_amount:
+		AMOUNT.small:
+			vfx_instance.amount = 50
+		AMOUNT.medium:
+			vfx_instance.amount = 100
+		AMOUNT.large:
+			vfx_instance.amount = 200
+	vfx_instance.process_material.emission_shape_offset = emission_shape.position
+	vfx_instance.process_material.emission_box_extents = emission_shape.shape.size
+	
+	for index in mesh_surface_indexes:
+		var material: StandardMaterial3D = weapon_mesh.mesh.surface_get_material(index)
+		material.albedo_color = enchant_dict[upgrade_type]["color"]
+	
+	#TODO - MAYBE:
 	#potentially apply (not yet implemented)shader with said color to mesh
